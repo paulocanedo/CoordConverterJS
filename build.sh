dc713c221d47b91ac26f18310e1fc735e25faafe
Paulo Canedo <paulocanedo@paulocanedo-PC.(none)>
2012-11-14 11:44:30 -0200

    fixed script to build dist files

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 JAVA_BIN='/home/paulocanedo/custom/lib/jre1.7.0_07/bin/java';
 COMPRESSOR='/home/paulocanedo/Downloads/builder/componentbuild/lib/yuicompressor/yuicompressor-2.4.2.jar';
-FULL_JS_FILE='CoordConverter-full.js';
-MIN_JS_FILE='CoordConverter-min.js';
+FULL_JS_FILE='dist/CoordConverter-full.js';
+MIN_JS_FILE='dist/CoordConverter-min.js';
 
 rm $FULL_JS_FILE
 rm $MIN_JS_FILE
-cat *.js > $FULL_JS_FILE
+cat src/*.js > $FULL_JS_FILE
 
 $JAVA_BIN -jar $COMPRESSOR $FULL_JS_FILE -o $MIN_JS_FILE
 
